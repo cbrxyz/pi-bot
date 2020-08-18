b69bc2da447b90fba9a7d7988c680abf954b4064
cbrxyz <me@cbrxyz.com>
2020-08-18 10:46:08 -0400

    Forgot version

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-version = "3.1.7"
+version = "3.1.8"
 developers = "Created and developed by <@715048392408956950>."
 repo = "https://github.com/cbrxyz/pi-bot"
 wikiLink = "https://scioly.org/wiki/index.php/User:Pi-Bot"
