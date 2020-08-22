a5e667e7e762b6c06a566d53a74e2221703be45a
cbrxyz <me@cbrxyz.com>
2020-08-22 13:21:08 -0400

    Made errors clearer

--- src/wiki/stylist.py
+++ src/wiki/stylist.py
@@ -16,15 +16,11 @@ async def prettifyTemplates():
         title = page.title()
         parsed = wtp.parse(text)
         for template in parsed.templates:
-            print(template)
-            print(template.normal_name())
-            print(template.pformat())
-            print(template.arguments)
             if len(template.arguments) > 3:
                 try:
                     text = text.replace(str(template), str(template.pformat()))
                 except Exception as e:
-                    print("ERROR:")
+                    print("ERROR on text replace:")
                     print(e)
         await setPageText(str(title), str(text), "Styled templates into pretty format. For any comments/concerns, please contact [[User:Pepperonipi]].", minor=True)
         await asyncio.sleep(20)
