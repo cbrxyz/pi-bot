1dc0ad35bdb49cab587e23684618115ead1dc85a
cbrxyz <me@cbrxyz.com>
2020-09-08 18:01:19 -0400

    Closes #126

--- bot.py
+++ bot.py
@@ -1405,7 +1405,7 @@ async def on_member_join(member):
     "You can add roles here, using the commands shown at the top of this channel. " + 
     "If you have any questions, please just ask here, and a helper or moderator will answer you ASAP." + 
     "\n\n" + 
-    "A helper or moderator will confirm you ASAP. You will then have access to the rest of the server to chat with other members!")
+    "**A helper or moderator will confirm you ASAP. You will then have access to the rest of the server to chat with other members!**")
     memberCount = len(member.guild.members)
     loungeChannel = discord.utils.get(member.guild.text_channels, name="lounge")
     if memberCount % 100 == 0:
