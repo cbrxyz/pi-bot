e585b696d450d9cd8916650a789b276b0904a287
cbrxyz <me@cbrxyz.com>
2020-09-27 17:47:19 -0400

    Updated welcome message

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-version = "4.3.3"
+version = "4.3.4"
 developers = "Created and developed by <@715048392408956950>."
 repo = "https://github.com/cbrxyz/pi-bot"
 wikiLink = "https://scioly.org/wiki/index.php/User:Pi-Bot"
