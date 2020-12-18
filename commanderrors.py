c6629a76a3d2b7f3ce843c41c092c7eb4cb2d1ad
Jareth Gomes <jarethgomes@gmail.com>
2020-12-18 16:05:59 -0600

    Created CommandNotInvokedInBotSpam command_error exception

--- commanderrors.py
+++ commanderrors.py
@@ -1,5 +1,13 @@
 import discord.ext.commands as commands
+
 class CommandNotAllowedInChannel(commands.CommandError):
     def __init__(self, channel, *args, **kwargs):
         self.channel = channel
+        super().__init__(*args, **kwargs)
+        
+        
+class CommandNotInvokedInBotSpam(commands.CommandError):
+    def __init__(self, channel, message, *args, **kwargs):
+        self.channel = channel
+        self.message = message
         super().__init__(*args, **kwargs)
