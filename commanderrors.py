5eb1f7773515ef9f2f4e2ad7b8f7373fbeab9160
Jareth Gomes <jarethgomes@gmail.com>
2020-12-18 13:31:55 -0600

    Renamed to better represent as command errors

--- commanderrors.py
+++ commanderrors.py
@@ -1,5 +1,5 @@
 import discord.ext.commands as commands
-class BlacklistException(commands.CommandError):
+class CommandNotAllowedInChannel(commands.CommandError):
     def __init__(self, channel, *args, **kwargs):
         self.channel = channel
         super().__init__(*args, **kwargs)
