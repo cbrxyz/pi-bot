99cb5173749dc960db3e50326ae86b51b2648f1c
cbrxyz <me@cbrxyz.com>
2021-11-06 14:19:09 -0400

    Created eventadd, eventremove commands

--- src/discord/staffcommands.py
+++ src/discord/staffcommands.py
@@ -1404,19 +1404,58 @@ class StaffNonessential(StaffCommands, name="StaffNonesntl"):
         await delete("data", "tags", tag['_id'])
         return await ctx.interaction.response.send_message(content = f"The `{tag_name}` tag was deleted.")
 
-    # @discord.commands.slash_command(
-    #     name = "eventadd",
-    #     description = "Staff command. Adds a new event."
-    # )
-    # async def event_add(self, ctx):
-    #     pass
+    @discord.commands.slash_command(
+        guild_ids = [SLASH_COMMAND_GUILDS],
+        name = "eventadd",
+        description = "Staff command. Adds a new event."
+    )
+    async def event_add(self,
+        ctx,
+        event_name: Option(str, "The name of the new event.", required = True),
+        event_aliases: Option(str, "The aliases for the new event. Format as 'alias1, alias2'.")
+        ):
+        if event_name in [e['name'] for e in src.discord.globals.EVENT_INFO]:
+            return await ctx.interaction.response.send_message(content = f"The `{event_name}` event has already been added.")
 
-    # @discord.commands.slash_command(
-    #     name = 'eventremove',
-    #     description = "Removes an event's availability and optionally, its role from all users."
-    # )
-    # async def event_remove(self, ctx):
-    #     pass
+        aliases_array = re.findall(r'\w+', event_aliases)
+        new_dict = {
+            'name': event_name,
+            'aliases': aliases_array
+        }
+
+        src.discord.globals.EVENT_INFO.append(new_dict)
+        await insert("data", "events", new_dict)
+
+        server = self.bot.get_guild(SERVER_ID)
+        await server.create_role(
+            name = event_name,
+            color = discord.Color(0x82A3D3),
+            reason = f"Created by {str(ctx.author)} using /eventadd with Pi-Bot."
+            )
+        await ctx.interaction.response.send_message(content = f"The `{event_name}` event was added.")
+
+    @discord.commands.slash_command(
+        guild_ids = [SLASH_COMMAND_GUILDS],
+        name = 'eventremove',
+        description = "Removes an event's availability and optionally, its role from all users."
+    )
+    async def event_remove(self, 
+        ctx,
+        event_name: Option(str, "The name of the event to remove.", required = True),
+        delete_role: Option(str, "Whether to delete the event role from all users. 'no' allows role to remain.", choices = ["no", "yes"], default = "no", required = True)
+        ):
+        if event_name not in [e['name'] for e in src.discord.globals.EVENT_INFO]:
+            return await ctx.interaction.response.send_message(content = f"The `{event_name}` event does not exist.")
+
+        if delete_role == "yes":
+            server = self.bot.get_guild(SERVER_ID)
+            role = discord.utils.get(server.roles, name = event_name)
+            await role.delete()
+
+        event = [e for e in src.discord.globals.EVENT_INFO if e['name'] == event_name][0]
+        src.discord.globals.EVENT_INFO.remove(event)
+        await delete("data", "events", event['_id'])
+        await ctx.interaction.response.send_message(content = f"The `{event_name}` event was deleted.")
 
 def setup(bot):
     bot.add_cog(StaffEssential(bot))
