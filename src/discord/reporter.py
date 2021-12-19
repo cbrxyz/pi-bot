7f4ca99285390ce35996d2730361e0d503b747dc
cbrxyz <me@cbrxyz.com>
2021-12-19 16:18:19 -0500

    Added Kick button in Reporter

--- src/discord/reporter.py
+++ src/discord/reporter.py
@@ -67,6 +67,35 @@ class ChangeInnapropriateUsername(discord.ui.Button):
         # Update the report database
         # TODO
 
+class KickUserButton(discord.ui.Button):
+
+    view: discord.ui.View
+
+    def __init__(self, view):
+        self.view = view
+        super().__init__(style = discord.ButtonStyle.red, label = "Kick User", custom_id = f"{view.report_id}:kick")
+
+    async def callback(self, interaction: discord.Interaction):
+        # Delete the original message
+        await interaction.message.delete()
+
+        # Check to make sure user is still in server before taking action
+        member_still_here = self.view.member in self.view.member.guild.members
+
+        # Send an informational message about the report being updated
+        closed_reports = discord.utils.get(interaction.guild.text_channels, name = 'closed-reports')
+        if member_still_here:
+            await closed_reports.send(f"**Member was kicked** by {interaction.user.mention} - {self.view.member.mention} had the innapropriate username `{self.view.offending_username}`, and the user was kicked from the server.")
+
+            # Kick the user
+            await self.view.member.kick()
+
+        else:
+            await closed_reports.send(f"**Attemped to kick member* by {interaction.user.mention} - {self.view.member.mention} had the innapropriate username `{self.view.offending_username}` and a kick was attempted on the user, however, the user had left the server.")
+
+        # Update the report database
+        # TODO
+
 class InnapropriateUsername(discord.ui.View):
 
     member: discord.Member
