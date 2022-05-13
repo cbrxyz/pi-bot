e6db5af3e5b81e596ac1df3ca599787832fb62fb
cbrxyz <me@cbrxyz.com>
2022-05-13 17:19:56 -0400

    Add docstrings, call super() init in module-specific errors

--- commanderrors.py
+++ commanderrors.py
@@ -1,18 +1,35 @@
+"""
+Stores various specific errors. These can be used to better describe why specific
+exceptions occurred, avoiding the need to use general exceptions.
+"""
 from discord.ext import commands
 
 
 class CommandNotAllowedInChannel(commands.CommandError):
+    """
+    There was an attempt to execute a command in a channel that the command was not
+    permitted to be executed in. Currently not used.
+    """
     def __init__(self, channel, *args, **kwargs):
         self.channel = channel
         super().__init__(*args, **kwargs)
 
 
 class CommandNotInvokedInBotSpam(commands.CommandError):
+    """
+    A command which was only permitted to be invoked in #bot-spam was invoked in
+    another channel. Currently not used.
+    """
     def __init__(self, channel, message, *args, **kwargs):
         self.channel = channel
         self.message = message
+        super().__init__(*args, **kwargs)
 
 
 class SelfMuteCommandStaffInvoke(commands.CommandError):
+    """
+    A staff member attempted to self-mute themselves. Currently not used.
+    """
     def __init__(self, message, *args, **kwargs):
         self.message = message
+        super().__init__(*args, **kwargs)
