ecad381bb5f52d09ab9f1d5615a4ac4017c3e833
cbrxyz <me@cbrxyz.com>
2022-05-15 14:49:38 -0400

    Move censor-related cog listeners in logger to censor cog, various formatting on logger cog

--- src/discord/censor.py
+++ src/discord/censor.py
@@ -11,7 +11,7 @@ import discord
 import src.discord.globals
 from discord.ext import commands
 from src.discord.globals import (CATEGORY_STAFF, CHANNEL_SUPPORT,
-                                 DISCORD_INVITE_ENDINGS)
+                                 DISCORD_INVITE_ENDINGS, ROLE_UC, CENSOR)
 
 if TYPE_CHECKING:
     from bot import PiBot
@@ -133,6 +133,125 @@ class Censor(commands.Cog):
 
         # Replace content with censored content for other cogs
         message.content = content
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before: discord.Message, after: discord.Message):
+        """
+        Listens for message edit events, and executes the censor if needed.
+
+        Args:
+            before (discord.Message): The message before it was edited.
+            after (discord.Message): The message after it was edited.
+        """
+        # Do not trigger the message edit event for newly-created messages
+        if (discord.utils.utcnow() - after.created_at).total_seconds() < 2:
+            return
+
+        # Log edit event
+        print(
+            "Message from {0.author} edited to: {0.content}, from: {1.content}".format(
+                after, before
+            )
+        )
+
+        # Stop the event here for DM's (no need to censor, as author is the
+        # only one who can see them)
+        if isinstance(after.channel, discord.DMChannel):
+            return
+
+        # Stop here for messages from Pi-Bot (no need to do anything else)
+        if after.author.bot:
+            return
+
+        # Delete messages that contain censored words
+        censor_found = self.censor_needed(after.content)
+        if censor_found:
+            await after.delete()
+            await after.author.send(
+                "You recently edited a message, but it **contained a censored "
+                "word**! Therefore, I unfortunately had to delete it. In the "
+                "future, please do not edit innapropriate words into your "
+                "messages, and they will not be deleted."
+            )
+
+        # Delete messages that have Discord invite links in them
+        discord_invite_found = self.discord_invite_censor_needed(after.content)
+        if discord_invite_found:
+            await after.delete()
+            await after.author.send(
+                "You recently edited a message, but it **contained a link to "
+                "another Discord server**! Therefore, I unfortunately had to "
+                "delete it. In the future, please do not edit Discord invite "
+                "links into your messages and they will not be deleted."
+            )
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        # Give new user confirmed role
+        unconfirmed_role = discord.utils.get(member.guild.roles, name=ROLE_UC)
+        assert isinstance(unconfirmed_role, discord.Role)
+        await member.add_roles(unconfirmed_role)
+
+        # Check to see if user's name is innapropriate
+        name = member.name
+        if self.censor_needed(name):
+            # If name contains a censored link
+            reporter_cog = self.bot.get_cog("Reporter")
+            await reporter_cog.create_inappropriate_username_report(member, member.name)
+
+    @commands.Cog.listener()
+    async def on_member_update(self, _, after):
+        """
+        When a member updates their presence on the Discord server, check to see
+        if their name contains an innapropriate term, and if so, open a report.
+
+        Args:
+            after (discord.Member): The member who changed their name, after they've
+                changed it.
+        """
+        # Notify staff if the user updated their name to include an innapropriate name
+        if after.nick is None:
+            return  # No need to check if user does not have a new nickname set
+
+        # Get the Censor cog
+        censor_found = self.censor_needed(after.nick)
+        if censor_found:
+            # If name contains a censored link
+            reporter_cog = self.bot.get_cog("Reporter")
+            await reporter_cog.create_inappropriate_username_report(after, after.nick)
+
+    @commands.Cog.listener()
+    async def on_user_update(self, _, after):
+        """
+        When a user updates their global Discord profile, check to see if their
+        name contains an inappropriate term, and if so, open a report.
+
+        Args:
+            after (discord.Member): The member after updating their profile.
+        """
+        # Get the Censor cog and see if user's new username is offending censor
+        censor_found = self.censor_needed(after.name)
+        if censor_found:
+            # If name contains a censored link
+            reporter_cog = self.bot.get_cog("Reporter")
+            await reporter_cog.create_inappropriate_username_report(after, after.name)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        """
+        Handles reaction add events. Currently, just used to suppress offensive emojis.
+
+        Args:
+            payload (discord.RawReactionActionEvent): The payload to use for the message.
+        """
+        if str(payload.emoji) in src.discord.globals.CENSOR["emojis"]:
+            channel = self.bot.get_channel(payload.channel_id)
+            assert isinstance(channel, discord.TextChannel)
+
+            partial_message = channel.get_partial_message(payload.message_id)
+            assert isinstance(partial_message, discord.PartialMessage)
+
+            await partial_message.clear_reaction(payload.emoji)
 
 
 async def setup(bot: PiBot):
